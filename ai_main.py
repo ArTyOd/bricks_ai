7a2f2e2698d57a5570cadb54632e6b1a44932258
ArTyOd <72732930+ArTyOd@users.noreply.github.com>
2023-11-01 16:38:08 +0100

    01.11. -1

--- ai_main.py
+++ ai_main.py
@@ -21,18 +21,20 @@ openai_api_key = st.secrets["OPENAI_API_KEY"]
 openai.api_key = openai_api_key
 
 # Create API client for Google Cloud Storage
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
+credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
 client = storage.Client(credentials=credentials)
 
 # Bucket name and file path
 bucket_name = "bucket_g_cloud_service_1"
 
-messages = [{"role": "system", "content": "Answer the question based on the context below, and if the question can't be answered based on the context, say \"I don't know\""}]
+messages = [
+    {
+        "role": "system",
+        "content": "Answer the question based on the context below, and if the question can't be answered based on the context, say \"I don't know\"",
+    }
+]
 selected_categories = []
 session_id = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-
 
 
 def save_chat_history(chat_history, filename):
@@ -46,12 +48,17 @@ def save_chat_history(chat_history, filename):
 def clear_chat_history():
     global messages, session_id
     session_id = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    messages = [{"role": "system", "content": "Answer the question based on the context below, and if the question can't be answered based on the context, say \"I don't know\""}]
+    messages = [
+        {
+            "role": "system",
+            "content": "Answer the question based on the context below, and if the question can't be answered based on the context, say \"I don't know\"",
+        }
+    ]
 
 
 def save_chat_log(chat_log, filename):
     folder_path = "log/"
-    with open(folder_path + filename, 'w') as f:
+    with open(folder_path + filename, "w") as f:
         json.dump(chat_log, f)
 
 
@@ -62,57 +69,56 @@ def count_tokens(text):
 
 
 def load_index():
-    pinecone.init(
-    api_key=pinecone_api_key,
-    environment=pinecone_environment
-)
-    index_name = 'bricks'
+    pinecone.init(api_key=pinecone_api_key, environment=pinecone_environment)
+    index_name = "bricks"
     if not index_name in pinecone.list_indexes():
         raise KeyError(f"Index '{index_name}' does not exist.")
     return pinecone.Index(index_name)
 
 
 def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301"):
-  """Returns the number of tokens used by a list of messages."""
-  try:
-      encoding = tiktoken.encoding_for_model(model)
-  except KeyError:
-      encoding = tiktoken.get_encoding("cl100k_base")
-  if model == "gpt-3.5-turbo-0301":  # note: future models may deviate from this
-      num_tokens = 0
-      for message in messages:
-          num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
-          for key, value in message.items():
-              num_tokens += len(encoding.encode(value))
-              if key == "name":  # if there's a name, the role is omitted
-                  num_tokens += -1  # role is always required and always 1 token
-      num_tokens += 2  # every reply is primed with <im_start>assistant
-      return num_tokens
-  else:
-      raise NotImplementedError(f"""num_tokens_from_messages() is not presently implemented for model {model}.
-  See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.""")
+    """Returns the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model == "gpt-3.5-turbo-0301":  # note: future models may deviate from this
+        num_tokens = 0
+        for message in messages:
+            num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
+            for key, value in message.items():
+                num_tokens += len(encoding.encode(value))
+                if key == "name":  # if there's a name, the role is omitted
+                    num_tokens += -1  # role is always required and always 1 token
+        num_tokens += 2  # every reply is primed with <im_start>assistant
+        return num_tokens
+    else:
+        raise NotImplementedError(
+            f"""num_tokens_from_messages() is not presently implemented for model {model}.
+  See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
+        )
 
 
-def create_context(question,index, max_len=1500, size="ada"):
+def create_context(question, index, max_len=1500, size="ada"):
     """
     Create a context for a question by finding the most similar context from the Pinecone index
     """
 
     # Get the embeddings for the question
-    q_embed = openai.Embedding.create(input=question, engine='text-embedding-ada-002')['data'][0]['embedding']
+    q_embed = openai.Embedding.create(input=question, engine="text-embedding-ada-002")["data"][0]["embedding"]
 
     # Query Pinecone index
     res = index.query(q_embed, filter={"category": {"$in": selected_categories}}, top_k=5, include_metadata=True)
-           
+
     context_details = []
     returns = []
     cur_len = 0
 
     # Iterate through results, sorted by score (ascending), and add the text to the context until the context is too long
-    for match in sorted(res['matches'], key=lambda x: x['score']):
+    for match in sorted(res["matches"], key=lambda x: x["score"]):
         # Get the length of the text
-        text_len = match['metadata']['n_tokens']
-        
+        text_len = match["metadata"]["n_tokens"]
+
         # Add the length of the text to the current length
         cur_len += text_len + 4
 
@@ -121,16 +127,19 @@ def create_context(question,index, max_len=1500, size="ada"):
             break
 
         # Else, add it to the text that is being returned
-        returns.append(match['metadata']['content'])
-        
+        returns.append(match["metadata"]["content"])
+
         # Add the context information to the context_details list
-        context_details.append({
-                'score': match.get('score', None),
-                'category': match['metadata'].get('category', None),
-                'topic': match['metadata'].get('topic', None),
-                'url': match['metadata'].get('url', None),
-                'token': match['metadata'].get('n_tokens', None),
-            })
+        context_details.append(
+            {
+                "score": match.get("score", None),
+                "category": match["metadata"].get("category", None),
+                "content": match["metadata"].get("content", None),
+                "topic": match["metadata"].get("topic", None),
+                "url": match["metadata"].get("url", None),
+                "token": match["metadata"].get("n_tokens", None),
+            }
+        )
 
     print(f"How much Contexts found: {len(returns)} \n----------------\n ")
     # Return the context and context_details
@@ -141,7 +150,7 @@ def fallback_reframe_question(messages, original_question, model="gpt-3.5-turbo"
     """
     Rephrase the original question to be more specific, given the context of the chat history (messages).
     """
- 
+
     # Create a copy of the original messages
     # messages_copy = messages.copy()
     messages_reframe = []
@@ -159,8 +168,8 @@ def fallback_reframe_question(messages, original_question, model="gpt-3.5-turbo"
             temperature=temperature,
             max_tokens=max_tokens,
         )
-        
-        rephrased_question = response["choices"][0]["message"]['content'].strip()
+
+        rephrased_question = response["choices"][0]["message"]["content"].strip()
         if "don't know" in rephrased_question:
             return original_question
         return rephrased_question
@@ -170,48 +179,48 @@ def fallback_reframe_question(messages, original_question, model="gpt-3.5-turbo"
         return ""
 
 
-def engineer_prompt(question, index, max_len,model, reframing = False):
+def engineer_prompt(question, index, max_len, model, reframing=False):
     """
     Answer a question based on the most similar context from the Pinecone index
     """
     global messages
-    
+
     context, context_details = create_context(question, index, max_len)
-    context_score = context_details[0]['score'] if context_details else 0
+    context_score = context_details[0]["score"] if context_details else 0
     if context_score < 0.78 and reframing:
         rephrased_question = fallback_reframe_question(messages, question, model=model, temperature=0.1, max_tokens=100)
         new_context, new_context_details = create_context(rephrased_question, index, max_len)
-        new_context_score = new_context_details[0]['score'] if context_details else 0
+        new_context_score = new_context_details[0]["score"] if context_details else 0
         if new_context_score > context_score:
             context, context_details = new_context, new_context_details
-            print(f"Context score to low {context_score = }, question was rephrased: {rephrased_question}, new context score {new_context_score} \n----------------\n")
+            print(
+                f"Context score to low {context_score = }, question was rephrased: {rephrased_question}, new context score {new_context_score} \n----------------\n"
+            )
             question = rephrased_question
-    prompt =  [ {"role": "assistant", "content": f"Context: {context}"},
-                {"role": "user", "content": f"{question}"}]
+    prompt = [{"role": "assistant", "content": f"Context: {context}"}, {"role": "user", "content": f"{question}"}]
     return prompt, context_details
 
 
 def answer_question(
     model="gpt-3.5-turbo",
-    instruction = "Answer the question based on the context below, and if the question can't be answered based on the context, say \"I don't know\"",
-    index = "",
-    categories = [],
-    question = "what?",
-    reframing = True,
+    instruction="Answer the question based on the context below, and if the question can't be answered based on the context, say \"I don't know\"",
+    index="",
+    categories=[],
+    question="what?",
+    reframing=True,
     size="ada",
     max_tokens=150,
     max_len=1500,
-    temperature = 0.5,
+    temperature=0.5,
     debug=False,
     stop=None,
-    callback=None):
-   
-
+    callback=None,
+):
     global messages, selected_categories
-    
+
     selected_categories = categories
     messages[0] = {"role": "system", "content": f"{instruction}"}
-    prompt, context_details = engineer_prompt(question= question,index = index, max_len = max_len, reframing=reframing, model=model)
+    prompt, context_details = engineer_prompt(question=question, index=index, max_len=max_len, reframing=reframing, model=model)
     messages += prompt
 
     print(f"messages before the prompting OpenAO: {messages} \n----------------\n ")
@@ -239,11 +248,10 @@ def answer_question(
                 output_text += r_text
                 if callback:
                     callback(r_text)
-        
-        
+
         if callback:
-            system_message = {"role" : "assistant" , "content" :  output_text}
-            
+            system_message = {"role": "assistant", "content": output_text}
+
             prompt_tokens = num_tokens_from_messages(messages)
             completion_tokens = num_tokens_from_messages([system_message])
             total_tokens = prompt_tokens + completion_tokens
@@ -254,14 +262,12 @@ def answer_question(
             save_chat_history(messages, chat_log_filename)
             print(f"{total_tokens =} {prompt_tokens =} { completion_tokens =}")
             return messages, context_details, prompt_tokens, completion_tokens, total_tokens
-        
-        return response["choices"][0]["message"]['content'], context_details
+
+        return response["choices"][0]["message"]["content"], context_details
     except Exception as e:
         print(e)
         print(f"something is wrong")
-        return "", "",  0, 0, 0
-
-
+        return "", "", 0, 0, 0
 
 
 # An Ai which helps to develop a long-term memory solution for large language models using external knowledge storage, vector embeddings, and sub-modules. The system should be independent of the language model and easily interchangeable. The focus is on creating an efficient and optimized memory structure that prevents slowdowns and increased expenses over time.
